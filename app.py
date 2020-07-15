d43603ce862d0f4d34fde286e211b170f90b0079
alkaf <>
2020-07-15 13:27:25 +0300

    add signin, logout with validate and session

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, json
+from flask import Flask, render_template, request, json, session, redirect
 from flaskext.mysql import MySQL
-from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.secret_key = 'alkaf'
+
 mysql = MySQL()
 
 # MySQL configurations
@@ -29,31 +30,75 @@ def signUp():
         _name = request.form['inputName']
         _email = request.form['inputEmail']
         _password = request.form['inputPassword']
+        conn = mysql.connect()
+        cursor = conn.cursor()
 
         if _name and _email and _password:
-            conn = mysql.connect()
-            cursor = conn.cursor()
 
             cursor.callproc('sp_createUser', (_name, _email, _password))
             data = cursor.fetchall()
 
             if len(data) is 0:
                 conn.commit()
+                conn.close()
                 return json.dumps({'message': 'User created successfully !'})
             else:
+                conn.close()
                 return json.dumps({'error': str(data[0])})
         else:
+            conn.close()
             return json.dumps({'html': '<span>Enter the required fields</span>'})
     except Exception as e:
         return json.dumps({'error': str(e)})
-    finally:
-        cursor.close()
-        conn.close()
+
 
 
 @app.route('/showSignin')
 def showSignin():
     return render_template('signin.html')
+
+
+@app.route('/validateLogin', methods=['POST'])
+def validateLogin():
+    try:
+        _username = request.form['inputEmail']
+        _password = request.form['inputPassword']
+
+        # connect to mysql
+
+        con = mysql.connect()
+        cursor = con.cursor()
+        cursor.callproc('sp_validateLogin', (_username,))
+        data = cursor.fetchall()
+
+        if len(data) > 0:
+            if str(data[0][3])== _password:
+                session['user'] = data[0][0]
+                con.close()
+                return redirect('/userHome')
+            else:
+                con.close()
+                return render_template('error.html', error='Wrong Email address or Password.')
+        else:
+            con.close()
+            return render_template('error.html', error='Wrong Email address or Password.')
+
+
+    except Exception as e:
+        return render_template('error.html', error=str(e))
+
+
+@app.route('/userHome')
+def userHome():
+    if session.get('user'):
+        return render_template('userHome.html')
+    else:
+        return render_template('error.html',error = 'Unauthorized Access')
+
+@app.route('/logout')
+def logout():
+    session.pop('user',None)
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(port = 5000)
